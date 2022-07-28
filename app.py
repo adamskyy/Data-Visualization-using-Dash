3e439013647001f82202014942bb7c450a2b444c
adamskyy <adam.ujma21@gmail.com>
2022-07-28 08:07:57 +0200

    Basic layout

--- app.py
+++ app.py
@@ -1,34 +1,79 @@
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
-from dash import Dash, html, dcc
-import plotly.express as px
+from dash import Dash, dcc, html, Input, Output, dash_table
 import pandas as pd
+import dash_bootstrap_components as dbc
 
-app = Dash(__name__)
+app = Dash(external_stylesheets=[dbc.themes.LUX])
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
+df = pd.read_csv('test.csv')
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+df.info()
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+# the style arguments for the sidebar. We use position:fixed and a fixed width
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "20rem",
+    "padding": "2rem 1rem",
+    "background-color": "#FF7300"
+}
 
-    html.Div(children='''
-        Dash: A web application framework for your data.
-    '''),
+# the styles for the main content position it to the right of the sidebar and
+# add some padding.
+CONTENT_STYLE = {
+    "margin-left": "20rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+    "background-color": "#939393",
+    "width": "auto",
+}
 
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
+sidebar = html.Div(
+    [
+        html.H6("Titanic dataset", className="display-6"),
+        html.Hr(),
+        html.P(
+            "Machine Learning from Disaster", className="lead"
+        ),
+        dbc.Nav(
+            [
+                dbc.NavLink("Home", href="/", active="exact"),
+                dbc.NavLink("Page 1", href="/page-1", active="exact"),
+                dbc.NavLink("Page 2", href="/page-2", active="exact"),
+            ],
+            vertical=True,
+            pills=True,
+        ),
+    ],
+    style=SIDEBAR_STYLE,
+)
+
+content = html.Div(id="page-content", style=CONTENT_STYLE)
+
+home = dash_table.DataTable(df.to_dict('records'), [{"name": i, "id": i} for i in df.columns],
+style_table={'height': '300px', 'overflowY': 'auto'})
+
+app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
+
+
+@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
+def render_page_content(pathname):
+    if pathname == "/":
+        return home
+    elif pathname == "/page-1":
+        return html.P("This is the content of page 1. Yay!")
+    elif pathname == "/page-2":
+        return html.P("Oh cool, this is page 2!")
+    # If the user tries to reach a different page, return a 404 message
+    return dbc.Jumbotron(
+        [
+            html.H1("404: Not found", className="text-danger"),
+            html.Hr(),
+            html.P(f"The pathname {pathname} was not recognised..."),
+        ]
     )
-])
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
